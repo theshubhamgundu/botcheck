62cada46800ee97f28c0392d9cda9b50261c90f4
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-04-28 09:18:56 +0530

    Update __init__.py

--- Hackfreaks/modules/sql/__init__.py
+++ Hackfreaks/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from Hackfreaks import DB_URI
+from EMCEE import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
