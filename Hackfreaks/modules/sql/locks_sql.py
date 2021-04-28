ceefe4a2766b7266bd7f9c6761c363142b9003a5
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-04-28 09:25:55 +0530

    Update locks_sql.py

--- Hackfreaks/modules/sql/locks_sql.py
+++ Hackfreaks/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from Hackfreaks.modules.sql import SESSION, BASE
+from EMCEE.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
