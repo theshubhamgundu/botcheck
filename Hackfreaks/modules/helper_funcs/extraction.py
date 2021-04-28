5d417e7c83d06cb916c7a0b9a462dab3233e945a
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-04-28 09:15:32 +0530

    Update extraction.py

--- Hackfreaks/modules/helper_funcs/extraction.py
+++ Hackfreaks/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from Hackfreaks import LOGGER
-from Hackfreaks.modules.users import get_user_id
+from EMCEE import LOGGER
+from EMCEE.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
