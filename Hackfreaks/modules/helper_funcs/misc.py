83650c99cae54989d198e014326108e3a6f719d8
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-04-28 09:17:18 +0530

    Update misc.py

--- Hackfreaks/modules/helper_funcs/misc.py
+++ Hackfreaks/modules/helper_funcs/misc.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from Hackfreaks import NO_LOAD
+from EMCEE import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
